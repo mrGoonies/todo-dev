8ab58c2f9cf2c89238bee484dd66820956d6d268
Gabriel Muñoz <93497614+mrGoonies@users.noreply.github.com>
2024-04-19 14:08:51 -0400

    Modularizando funcionalidades

---
+++ static/js/task.js
@@ -0,0 +1,11 @@
+class Task {
+    constructor(name, description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    showAllTask = () => {
+        console.log(this.name)
+    }
+}
+
