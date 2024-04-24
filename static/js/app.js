fc55563eaa1a4a873add5a5fca849d38d679c99e
Gabriel Muñoz <93497614+mrGoonies@users.noreply.github.com>
2024-04-23 23:43:04 -0400

    Creando usuarios

--- static/js/app.js
+++ static/js/app.js
@@ -1,74 +1,43 @@
-function menu() {
-  let opt = 0;
-  alert("Ingresa una de las siguientes opciones del menú.");
-
-  do {
-    opt = parseInt(prompt("1) Ingresar tarea\n2) Modificar tarea\n3) Visualizar tarea\n4) Eliminar tarea\n5) Salir"));
-
-    switch (opt) {
-      case 1:
-        console.log(opt);
-        break;
-      case 2:
-        console.log(opt);
-        break;
-      case 3:
-        console.log(opt);
-        break;
-      case 4:
-        console.log(opt);
-        break;
-      case 5:
-        alert("Muchas gracias por usar ToDo Dev");
-        break;
-      default:
-        alert("Debes ingresar una opcion valida!");
-        break;
-    } 
-  
-  } while(opt != 5);
-}
-
-/**
- * Validamos el prompt ingresado por el usuario.
- * @param {number} numberOfTask 
- * @returns {number} El argumento pasado a la función validado.
- */
-function validateQuantityTask(numberOfTask) {
-  if (isNaN(numberOfTask)) {
-    alert("Debes ingresar un valor de tipo numérico");
-
-    return getQuantityTask();
+class User {
+  constructor (name, pwd, id) {
+    this.id = id;
+    this.name = name;
+    this.pwd = pwd;
   }
 
-  if (numberOfTask <= 0) {
-    alert("Debes ingresar una cantidad mayor a cero para poder almacenar tus tareas.");
-    return getQuantityTask();
+  getData = () => {
+    console.log(`=== Tu info ===\n- Usuario: ${this.name}\n- ID: ${this.id}`);
   }
-
-  return numberOfTask;
 }
 
-/**
- * Le solicitamos al usuario que ingrese la cantidad de tareas que almacenará en la app.
- * @returns {number} Total de tareas a almacenar previamente validadas.
- */
-function getQuantityTask() {
-  let numberTask = parseInt(prompt("Ingrese la cantidad de tareas que ingresarás en la app."));
-  let numberTaskValidate = validateQuantityTask(numberTask);
-
-  return numberTaskValidate;
+class Task {
+  constructor (id, name, description) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+  }
 }
 
+
+let userInfo = [];
+
 /**
- * Función principal del proyecto o entry point
+ * Creamos un usuario y lo instanciamos para posteriormente agregarlo a un array de info de usuarios. 
  */
+function createUser() {
+  const currentId = userInfo.length + 1;
+  const name = prompt("Ingresa tu nombre de usuario");
+  const pwd = prompt("Ingresa tu password");
+  const newUser = new User(name, pwd, currentId);
+
+  userInfo.push(newUser);
+  console.log(userInfo);
+  console.log("Usuario creado");
+}
+
+
 function main() {
-  alert("Bienvenido a ToDo Dev");
-  const totalTaks = getQuantityTask();
-  alert("Ingresarás una cantidad de tareas totales a :\n" + totalTaks);
-
-  menu();
+  createUser();
 }
 
 main();
