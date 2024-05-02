6b65145f9993fce662fb341ed847e3756a46fe47
Gabriel Muñoz <93497614+mrGoonies@users.noreply.github.com>
2024-05-01 21:01:41 -0400

    HoF en deleteTask Method

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 class User {
-  constructor (name, id) {
+  constructor(name, id) {
     this.id = id;
     this.name = name;
     this.task = [];
@@ -17,24 +17,23 @@ class User {
 
     if (tasks.length === 0) {
       alert("Usted no posee tareas almacenadas");
-      return ;
+      return;
     }
 
     for (const task of tasks) {
-      console.log(task)
-      alert(`=== Tarea ${task.id} ===\nNombre: ${task.name}\nDescripcion: ${task.description}`);
+      console.log(task);
+      alert(
+        `=== Tarea ${task.id} ===\nNombre: ${task.name}\nDescripcion: ${task.description}`
+      );
     }
-
   }
 
   addTask(name, description) {
-    this.task.push(
-      {
-        id: (this.task.length+1),
-        name: name,
-        description: description
-      }
-    )
+    this.task.push({
+      id: this.task.length + 1,
+      name: name,
+      description: description,
+    });
     console.log("Su tarea a sido insertada correctamente");
   }
 
@@ -43,46 +42,45 @@ class User {
 
     if (arrOfTask.length === 0) {
       alert("Usted no posee tareas almacenadas.");
-      return ;
+      return;
     }
 
-    for (const task of arrOfTask) {
-      if (task.id === idTask) {
-        console.log(task)
-        alert(`=== Tarea ${task.id} ===\nNombre: ${task.name}\nDescripcion: ${task.description}`);
-        break;
-      } else {
-        alert(`Lo siento, no posees ninguna tarea almacenada con un ID igual a ${idTask}`);
+    const filteredTask = arrOfTask.filter((task) => task.id === idTask);
+
+    if (filteredTask) {
+      for (const task of filteredTask) {
+        alert(
+          `=== Tarea ${task.id} ===\nNombre: ${task.name}\nDescripcion: ${task.description}`
+        );
       }
+      return;
     }
+
+    alert("Tu tarea no se a encontrado en nuestro To Do");
   }
 
   deleteTaskById(idTask) {
-    const arrOfTask = this.task;
+    const taskList = this.findTaskById(idTask);
     idTask -= 1;
 
-    if (arrOfTask.length === 0) {
-      alert("Usted no posee tareas almacenadas.");
-      return ;
-    }
-
-    if (idTask > arrOfTask.length || idTask < 0) {
+    if (idTask > this.task.length || idTask < 0) {
       alert("Haz excedido el rango de la cantidad de tareas");
-      return ;
+      return;
     }
 
     console.log(`Se eliminara el siguiente elemento ${this.task[idTask]}`);
     this.task.splice(idTask, 1);
-
     console.log(`Elemento con el indice ${idTask} a sido eliminado!`);
     console.log(this.task);
+
+    return;
   }
 }
 
 let userInfo = [];
 
 /**
- * Creamos un usuario y lo instanciamos para posteriormente agregarlo a un array de info de usuarios. 
+ * Creamos un usuario y lo instanciamos para posteriormente agregarlo a un array de info de usuarios.
  */
 function createUser() {
   const currentId = userInfo.length + 1;
@@ -94,44 +92,49 @@ function createUser() {
   console.log("Usuario creado");
 }
 
+/**
+ * Funcion principal del programa o entry point de la app
+ */
 function main() {
   createUser();
   let firstObj = userInfo[0];
-  
-  console.log(firstObj)
+
+  console.log(firstObj);
   alert(`Bienvenido ${firstObj.name} a ToDo Dev`);
-  
+
   let opt = 0;
 
   do {
-    opt = parseInt(prompt("Elige una de las siguientes opciones:\n1) Agregar tarea.\n2) Mostar todas las tareas\n3) Mostrar una tarea por ID\n4) Eliminar Tarea\n5) Salir"));
-    
+    opt = parseInt(
+      prompt(
+        "Elige una de las siguientes opciones:\n1) Agregar tarea.\n2) Mostar todas las tareas\n3) Mostrar una tarea por ID\n4) Eliminar Tarea\n5) Salir"
+      )
+    );
+
     switch (opt) {
       case 1:
         let nameTask = prompt("Ingresa el nombre de la tarea");
         let description = prompt("Ingresa una descripcion de tu tarea");
-        
+
         firstObj.addTask(nameTask, description);
         console.log(firstObj);
         break;
-      
+
       case 2:
         firstObj.allTask;
         break;
-      
+
       case 3:
         let idForTask = parseInt(prompt("Ingresa el ID de tu tarea"));
-        
-        if (idForTask > 0) {
-          firstObj.findTaskById(idForTask);
-          break;
-        }
 
-        alert("Debes ingresar un ID Valido");
+        firstObj.findTaskById(idForTask);
+
         break;
 
       case 4:
-        let idTaskForDelete = parseInt(prompt("Ingrese el ID de la tarea el cualquiere eliminar"))
+        let idTaskForDelete = parseInt(
+          prompt("Ingrese el ID de la tarea el cualquiere eliminar")
+        );
         firstObj.deleteTaskById(idTaskForDelete);
         alert("Se a eliminado tu tarea!");
 
@@ -139,13 +142,12 @@ function main() {
       case 5:
         alert("Muchas gracias por usar ToDo Dev!");
         break;
-      
+
       default:
         alert("Haz ingresado una opcion incorrecta, vuelve a intentar!");
         break;
     }
-  } 
-  while (opt != 5);
+  } while (opt != 5);
 }
 
 main();
